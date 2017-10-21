73a4d603ed2959c32c51cc13ce678d1ae166d72a
Venkata <venkata@yombu.com>
2017-10-20 20:50:37 -0400

    Image upload

--- Sohail_Assignment.py
+++ Sohail_Assignment.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import boto3
 from flask_mysqldb import MySQL
 from PIL import Image
 import glob
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -18,22 +20,22 @@ s3_resource = boto3.resource('s3')
 # port = response['DBInstances'][0]['Endpoint']['Port']
 # username = response['DBInstances'][0]['MasterUsername']
 # PASSWORD = "12345678"
-
-path = "/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/"
-pre_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/pre/*")
-post_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/*")
-for image in pre_images_list:
-    image_file = Image.open(image)
-    image_file = image_file.convert('1')
-    image = image.split("/")
-    image_file.save(path + image[-1])
-
-post_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/*")
-for i in pre_images_list:
-    s3_resource.meta.client.upload_file(i, 'pre-processed-kc', i.split("/")[-1])
-
-for i in post_images_list:
-    s3_resource.meta.client.upload_file(i, 'post-porcessed-kc', i.split("/")[-1])
+#
+# path = "/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/"
+# pre_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/pre/*")
+# post_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/*")
+# for image in pre_images_list:
+#     image_file = Image.open(image)
+#     image_file = image_file.convert('1')
+#     image = image.split("/")
+#     image_file.save(path + image[-1])
+#
+# post_images_list = glob.glob("/home/krishna/PycharmProjects/Sohail_Assignment/static/images/post/*")
+# for i in pre_images_list:
+#     s3_resource.meta.client.upload_file(i, 'pre-processed-kc', i.split("/")[-1])
+#
+# for i in post_images_list:
+#     s3_resource.meta.client.upload_file(i, 'post-porcessed-kc', i.split("/")[-1])
 
 @app.route('/')
 def index():
@@ -80,9 +82,27 @@ def gallery():
 
 @app.route('/submit')
 def submit():
+    return render_template("mnoormuh_pg4.html")
 
-    pass
 
+@app.route('/upload_data', methods=['POST'])
+def upload():
+    email = request.form['email']
+    phone = request.form['Phone']
+    image = request.form['file']
+
+    pre_image_name = image.split("/")[-1]
+    s3_resource.meta.client.upload_file(image, 'pre-processed-kc', pre_image_name)
+    image_file = Image.open(image)
+    image_file = image_file.convert('1')
+
+    #Change krishna to ubuntu
+    post_image_name = "post_" + pre_image_name
+    image_file.save("/home/krishna/" + post_image_name)
+    s3_resource.meta.client.upload_file(image, 'post-porcessed-kc', pre_image_name)
+
+
+    return image
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
